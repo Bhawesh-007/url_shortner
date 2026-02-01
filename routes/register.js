5277b39894ad6d3a46582c4d4f58d5758f0d2363
Bhawesh <bhaweshjoshi689@gmail.com>
2026-02-02 00:56:02 +0530

    made the register route for the user

---
+++ routes/register.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const User = require('../model/user');
+// to hash password 
+const bcrypt = require("bcrypt");
+
+router.route('/register' , async(req,res)=>{
+   //first always take what is incoming in the request
+   const {username , email , password} = req.body;
+   if(!username||!email||!password)return res.status(401).json({message : "Invalid data"});
+   //hash password
+   try{
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+   const newUser =  new User({
+     username : username,
+     email : email,
+     password : hashedPassword,
+   });
+   await newUser.save();
+   return res.status(201).json({
+    message : "Successfully registered",
+    data : newUser,
+   })
+}
+catch(err){
+    res.status(401).json({
+        status: "cant register",
+        message : err.message
+    })
+}
+
+})
